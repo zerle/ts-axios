2717ca6dd7782d4c1a8764afec4b009c2a749810
zhanglei <1163494244@qq.com>
2022-01-17 14:20:28 +0800

    搭建node服务，编写基本请求代码

---
+++ examples/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const router = express.Router()
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpackConfig = require('./webpack.config')
+
+const app = express()
+const compiler = webpack(webpackConfig)
+
+router.get('/simple/get', (req, res) => {
+  res.json({
+    msg: `hello world`
+  })
+})
+
+app.use(router)
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = process.env.PORT || 8080
+
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+})
